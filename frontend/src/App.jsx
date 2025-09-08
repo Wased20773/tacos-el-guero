bbe477ac4ba036edd34a31fafa51da7251d1dc88
Bryan Vazquez <gamer.brybry@gmail.com>
2025-09-08 12:58:54 -0700

    Added responsive design to the modals for the order numbers in the menu. Switched out borders for box shadows for a more modern design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -59,6 +59,8 @@ function App() {
       </Routes>
 
       {/* Currently fixes page to keep footer at bottom */}
+      <div></div>
+
       <div className="homepage-divider-for-footer"></div>
       <Footer />
 
